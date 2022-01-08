b5d144460c46a0c43cada1abfa84715ba432805e
2300652404@qq.com <2300652404@qq.com>
2022-01-08 23:15:45 +0800

    a

--- soutipai_app/test/widget_test.dart
+++ soutipai_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:soutipai_app/main.dart';
+import 'package:soutipai_app/UI/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
