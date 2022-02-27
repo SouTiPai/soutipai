2e7859c8caa92cd410d0e8da435477497cd16581
wutong <2300652404@qq.com>
2022-02-27 22:39:20 +0800

    添加CLass至JSON类型转换

--- soutipai_app/lib/main.dart
+++ soutipai_app/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/photograph_page',
+      initialRoute: '/result_page',
       onGenerateRoute: onGenerateRoute,
     );
   }
