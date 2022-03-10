6bf05f8621b2c80b79da7fe613447efe7c4b725d
wutong <2300652404@qq.com>
2022-03-10 17:26:00 +0800

    修正app错误与警告

--- soutipai_app/lib/common/route.dart
+++ soutipai_app/lib/common/route.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:soutipai_app/ui/crop_picture.dart';
 import 'package:soutipai_app/ui/home.dart';
+import 'package:soutipai_app/ui/more.dart';
+import 'package:soutipai_app/ui/photograph.dart';
 import 'package:soutipai_app/ui/result.dart';
 import 'package:soutipai_app/ui/wrong_questions_book.dart';
-import 'package:soutipai_app/ui/more.dart';
-import 'package:soutipai_app/ui/crop_picture.dart';
-import 'package:soutipai_app/ui/photograph.dart';
 
 final routes = {
-  "/": (context) => MyHomePage(title: "搜题派"),
-  "/result_page": (context) => ResultPage(),
-  "/wrong_questions_book_page":(context)=>WrongQuestionsBook(),
-  "/more_page":(context)=>more(),
-  "/crop_picture_page":(context)=>ScreenDemo(),
-  "/photograph_page":(context)=>ImagePickerWidget(),
-  "/home_page":(context)=>MyHomePage(title: "搜题派"),
+  "/": (context) => const MyHomePage(title: "搜题派"),
+  "/result_page": (context) => const ResultPage(),
+  "/wrong_questions_book_page": (context) => const WrongQuestionsBook(),
+  "/more_page": (context) => const more(),
+  "/crop_picture_page": (context) => const ScreenDemo(),
+  "/photograph_page": (context) => const ImagePickerWidget(),
+  "/home_page": (context) => const MyHomePage(title: "搜题派"),
 };
 
+// ignore: prefer_function_declarations_over_variables
 var onGenerateRoute = (RouteSettings settings) {
   // 具体处理
   final String? name = settings.name;
   final Function pageContentBuilder = routes[name] as Function;
+  // ignore: unnecessary_null_comparison
   if (pageContentBuilder != null) {
     if (settings.arguments != null) {
       final Route route = MaterialPageRoute(
