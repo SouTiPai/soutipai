f2f33963183840c6efc34d9e9809fae7800f68a5
wutong <2300652404@qq.com>
2022-03-27 15:31:32 +0800

    result.dart 跳转逻辑修改

--- soutipai_app/lib/main.dart
+++ soutipai_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/crop_picture_page',
+      initialRoute: '/photograph_page',
       onGenerateRoute: onGenerateRoute,
     );
   }
