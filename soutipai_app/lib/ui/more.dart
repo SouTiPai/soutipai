56f67f510c2b190c9e1e28d8fbc0e2926e8b610b
liurunxuan <94226565+liurunxaun@users.noreply.github.com>
2022-04-17 00:47:23 +0800

    更多功能页面完善

--- soutipai_app/lib/ui/more.dart
+++ soutipai_app/lib/ui/more.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class more extends StatefulWidget {
   const more({Key? key}) : super(key: key);
@@ -15,73 +16,102 @@ class _moreState extends State<more> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              "更多功能",
-              style: TextStyle(
-                //fontFamily:
-                fontSize: 30,
-              ),
-            ),
-            ActionChip(
-              onPressed: () {},
-              avatar: CircleAvatar(
-                backgroundColor: Colors.blue,
-                child: Text("错"),
-              ),
-              label: Text("错题本"), // 文本 Widget
-              labelStyle:
-              TextStyle(color: Colors.red, fontSize: 30), // 文本样式，TextStyle
-              labelPadding: EdgeInsets.fromLTRB(15, 50, 0, 50), // 文本外边距 Padding
-
-              // Chip 形状
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  width: 2,
-                  color: Colors.red,
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.all(10),
+              child: Text(
+                "更多功能",
+                style: TextStyle(
+                  fontFamily: "LiShu",
+                  fontSize: 65,
                 ),
               ),
             ),
-            ActionChip(
-              onPressed: () {},
-              avatar: CircleAvatar(
-                backgroundColor: Colors.blue,
-                child: Text("视"),
-              ),
-              label: Text("视频讲解"), // 文本 Widget
-              labelStyle:
-              TextStyle(color: Colors.red, fontSize: 30), // 文本样式，TextStyle
-              labelPadding: EdgeInsets.fromLTRB(15, 50, 0, 50), // 文本外边距 Padding
-
-              // Chip 形状
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  width: 2,
-                  color: Colors.red,
+            Container(
+              margin: EdgeInsets.all(10),
+              child: ActionChip(
+                backgroundColor: Colors.white,
+                onPressed: () {},
+                avatar: CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: Text("错"),
+                ),
+                label: Text("错题本 "), // 文本 Widget
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontFamily: "LiShu",
+                ), // 文本样式，TextStyle
+                labelPadding:
+                EdgeInsets.fromLTRB(20, 48, 50, 50), // 文本外边距 Padding
+                // Chip 形状
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(
+                    width: 2,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-            ActionChip(
-              onPressed: () {},
-              avatar: CircleAvatar(
-                backgroundColor: Colors.blue,
-                child: Text("错"),
-              ),
-              label: Text("ActionChip"), // 文本 Widget
-              labelStyle:
-              TextStyle(color: Colors.red, fontSize: 30), // 文本样式，TextStyle
-              labelPadding: EdgeInsets.fromLTRB(15, 50, 0, 50), // 文本外边距 Padding
+            Container(
+              margin: EdgeInsets.all(10),
+              child: ActionChip(
+                backgroundColor: Colors.white,
+                //padding: EdgeInsets.fromLTRB(30, 30, 10, 5),
+                onPressed: () {},
+                avatar: CircleAvatar(
+                  backgroundColor: Colors.green,
+                  child: Text("视"),
+                ),
+                label: Text("视频讲解"), // 文本 Widget
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontFamily: "LiShu",
+                ), // 文本样式，TextStyle
+                labelPadding:
+                EdgeInsets.fromLTRB(23, 50, 23, 50), // 文本外边距 Padding
 
-              // Chip 形状
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  width: 2,
-                  color: Colors.red,
+                // Chip 形状
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(
+                    width: 2,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: ActionChip(
+                backgroundColor: Colors.white,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                onPressed: () {},
+                avatar: CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: Text("我"),
+                ),
+                label: Text("我的   "), // 文本 Widget
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontFamily: "LiShu",
+                ), // 文本样式，TextStyle
+                labelPadding:
+                EdgeInsets.fromLTRB(45, 45, 60, 40), // 文本外边距 Padding
+
+                // Chip 形状
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(
+                    width: 2,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
